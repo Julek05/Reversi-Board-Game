03a9661d6775932d15fc7cb29690484663c7f45c
Julek05 <julian95przedwojski@gmail.com>
2020-11-08 02:00:48 +0100

    add typing by interface to parents of game component

--- game/src/ComputerGame.tsx
+++ game/src/ComputerGame.tsx
@@ -1,8 +1,13 @@
 import React, {FunctionComponent} from 'react'
 import Game from './Game'
 
+interface GameProps {
+    computerMode: boolean,
+    selfTeaching: boolean
+}
+
 export const ComputerGame: FunctionComponent = () => {
-    const gameProps: object = {
+    const gameProps: GameProps = {
         computerMode: true,
         selfTeaching: false
     }
--- game/src/PlayerGame.tsx
+++ game/src/PlayerGame.tsx
@@ -1,8 +1,13 @@
 import React, {FunctionComponent} from 'react'
 import Game from './Game'
 
+interface GameProps {
+    computerMode: boolean,
+    selfTeaching: boolean
+}
+
 export const PlayerGame: FunctionComponent = () => {
-    const gameProps: object = {
+    const gameProps: GameProps = {
         computerMode: false,
         selfTeaching: false
     }
